30b33f1910a9adb8e959c59ff651a05eb11e8394
portk <kunhee0713@gmail.com>
2023-12-08 15:34:50 +0900

    modify GetDataFromGov.js

--- React_files/src/components/GetDataFromGov.js
+++ React_files/src/components/GetDataFromGov.js
@@ -1,43 +1,28 @@
-import React, { useEffect, useState } from "react";;
+import React from "react";;
 
 function GetDataFromGov(){
-    const [chargerMap,setChargerMap] = useState();
-
     const getChargerMap = async () => {
         const myCode="Encoding 인증키 입력";
         const numOfRows="9999";
-        let pageNo="28" //안전상 수동으로 바꿔서 하였다.
-        let url = `http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?serviceKey=${myCode}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataType=JSON`;
-        const request = await fetch(url)
-        const response = await request.json();
-        setChargerMap(response.items.item);
+        for (let i=1; i < 29 ; i++){
+            let url = `http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?serviceKey=${myCode}&numOfRows=${numOfRows}&pageNo=${i}&dataType=JSON`;
+            const request = await fetch(url)
+            const response = await request.json();
+            const result = await response.items.item;
+            const url2="http://localhost:8080/test";
+            const request2 = await fetch(url2, {
+                method:"Post",
+                headers:{"Content-Type" : "application/json"},
+                body:JSON.stringify(result)
+            });
+            const response2 = await request2.text();
+            console.log(i+response2); //진행도를 확인하기 위한 로그
+        }
     }
-
-    const sendDataToBack = async () => {
-        const url="http://localhost:8080/test";
-        const request = await fetch(url, {
-            method:"Post",
-            headers:{"Content-Type" : "application/json"},
-            body:JSON.stringify(chargerMap)
-        });
-        const response = await request.text();
-        alert(response)
-    }
-
-    let ready=""
-    if (chargerMap != null){
-        console.log(chargerMap);
-        ready="준비됐습니다!"
-    }
-
-    useEffect(()=>{
-        getChargerMap()
-    },[])
 
     return (
         <div>
-            <p>{ready}</p>
-            <button onClick={() => {sendDataToBack()}}>전송하기</button>
+            <button onClick={() => {getChargerMap()}}>전송하기</button>
         </div>
     );
 }
