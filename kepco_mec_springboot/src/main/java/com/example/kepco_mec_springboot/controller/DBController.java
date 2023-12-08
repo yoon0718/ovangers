bc2b6d3b39aff79b50f2465e678ce0c536541eb1
bigzero3949 <bigzero3949@gmail.com>
2023-12-08 17:22:53 +0900

    springboot

--- kepco_mec_springboot/src/main/java/com/example/kepco_mec_springboot/controller/DBController.java
+++ kepco_mec_springboot/src/main/java/com/example/kepco_mec_springboot/controller/DBController.java
@@ -8,33 +8,86 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.kepco_mec_springboot.model.BusiId;
+import com.example.kepco_mec_springboot.model.ChargerMap;
 import com.example.kepco_mec_springboot.model.ChgerType;
 import com.example.kepco_mec_springboot.model.User;
 import com.example.kepco_mec_springboot.repository.BusiIdRepository;
+import com.example.kepco_mec_springboot.repository.ChargerMapRepository;
 import com.example.kepco_mec_springboot.repository.ChgerTypeRepository;
+import com.example.kepco_mec_springboot.repository.UserRepository;
 
 @RestController
 public class DBController {
 // User 테이블
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ChargerMapRepository chargerMapRepository;
+
     // 로그인
     @PostMapping("/api/login")
-    public List<> login() {
-        return "";
+    public String login(
+        @RequestParam("userId") String userId,
+        @RequestParam("userPassword") String userPassword
+    ) {
+        List<User> loginInfo = userRepository.findByUserIdAndUserPassword(userId,userPassword);
+        if (loginInfo.size() > 0) {
+            return "로그인 성공";
+        }
+        else {
+            return "로그인 실패\n아이디와 비밀번호를 다시 입력해주세요";
+        }
     }
 
     // 회원가입
     @PostMapping("/api/account")
-    public List<User> insertAccount() {
-        return "";
+    public String insertAccount(
+        @RequestParam("userId") String userId,
+        @RequestParam("userPassword") String userPassword,
+        @RequestParam("userEmail") String userEmail,
+        @RequestParam("userNickname") String userNickname,
+        @RequestParam("userTelephone") String userTelephone
+    ) {
+        User userInfo = new User();
+        userInfo.setUserId(userId);
+        userInfo.setUserPassword(userPassword);
+        userInfo.setUserEmail(userEmail);
+        userInfo.setUserNickname(userNickname);
+        userInfo.setManagerCheck("user");
+        userInfo.setUserTelephone(userTelephone);
+        userInfo.setUserPoint(0);
+        userRepository.save(userInfo);
+
+        return "회원가입 성공";
     }
 
     // 회원 정보 수정
     @PutMapping("/api/account")
-    public List<> updateAccount() {
-        return "";
+    public String updateAccount(
+        @RequestParam("userId") String userId,
+        @RequestParam("userPassword") String userPassword,
+        @RequestParam("userEmail") String userEmail,
+        @RequestParam("userNickname") String userNickname,
+        @RequestParam("userTelephone") String userTelephone
+    ) {
+        String grade = userRepository.findByUserId(userId).getManagerCheck();
+        int point = userRepository.findByUserId(userId).getUserPoint();
+
+        User userInfo = new User();
+        userInfo.setUserId(userId);
+        userInfo.setUserPassword(userPassword);
+        userInfo.setUserEmail(userEmail);
+        userInfo.setUserNickname(userNickname);
+        userInfo.setManagerCheck(grade);
+        userInfo.setUserTelephone(userTelephone);
+        userInfo.setUserPoint(point);
+        userRepository.save(userInfo);
+
+        return "회원 정보 수정 성공";
     }
 
     // 회원 탈퇴
@@ -45,8 +98,9 @@ public class DBController {
 
     // 비밀번호 찾기
     @GetMapping("/api/account")
-    public List<> selectAccount() {
-        return "";
+    public String selectAccount(@RequestParam("userId") String userId) {
+        String check = userRepository.findByUserId(userId).getManagerCheck();
+        return check;
     }
 
 // Search 테이블
@@ -57,7 +111,7 @@ public class DBController {
     }
 
     // 검색 기록
-    @GetMapping("/api/search-list")
+    @GetMapping("/api/search")
     public List<> searchList() {
         return "";
     }
@@ -82,7 +136,24 @@ public class DBController {
     }
 
 // ChargeMap 테이블
-    // 
+    // 지도 보기
+    @GetMapping("/api/map")
+    public List<> map() {
+        return "";
+    }
+
+    // 지도 범위 설정
+    @GetMapping("/api/location")
+    public List<ChargerMap> location(
+        @RequestParam("lat_start") float lat_start,
+        @RequestParam("lat_end") float lat_end,
+        @RequestParam("lng_start") float lng_start,
+        @RequestParam("lng_end") float lng_end
+    ) {
+        List<ChargerMap> locations = chargerMapRepository.findAllByLatBetweenAndLngBetween(lat_start, lat_end, lng_start, lng_end);
+
+        return locations;
+    }
 
 // ChargerReport 테이블
     // 고장 신고 목록
@@ -96,8 +167,4 @@ public class DBController {
     public List<> updateDown() {
         return "";
     }
-
-    
-    
-
 }
