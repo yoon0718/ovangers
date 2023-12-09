6f112b476eeade472ef5d32eaefddd177afbfe99
portk <kunhee0713@gmail.com>
2023-12-09 20:52:11 +0900

    we can use pin!

--- React_files/src/components/KakaoMap.js
+++ React_files/src/components/KakaoMap.js
@@ -1,49 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const {kakao} = window;
 function KakaoMap(){
     const [lat, setLat] = useState(34.6785);
     const [lng, setLng] = useState(126.736);
     const [chargerData,setChargerData] = useState([]);
-    const [map, setMap] = useState();
+    const [map, setMapData] = useState();
+    const [markers, setMarker] = useState([]);
     
-    const getChargerData = async (latStart,latEnd,lngStart,lngEnd) =>{
-        let url = `http://10.10.21.64:8080/api/location?lat_start=${latStart}&lat_end=${latEnd}&lng_start=${lngStart}&lng_end=${lngEnd}`;
-        const request = await fetch(url);
-        const response = await request.json();
-        setChargerData(response);
+    const getChargerData = async (bounds) =>{
+        if (bounds != null) {
+            let latStart = bounds.qa;
+            let latEnd = bounds.pa;
+            let lngStart = bounds.ha;
+            let lngEnd = bounds.oa;
+            let url = `http://10.10.21.64:8080/api/location?lat_start=${latStart}&lat_end=${latEnd}&lng_start=${lngStart}&lng_end=${lngEnd}`;
+            const request = await fetch(url);
+            const response = await request.json();
+            if (chargerData !== response){
+                setChargerData(response);
+            }
+        }
     }
 
-    const createMap = async () => {
+    const createMap = () => {
         let container = document.getElementById("map");
         let options = {
             center: new kakao.maps.LatLng(lat, lng),
             level: 3
         };
         let map = new kakao.maps.Map(container,options);
-        setMap(map);
+        setMapData(map);
         let zoomControl = new kakao.maps.ZoomControl();
         let typeControl = new kakao.maps.MapTypeControl();
         map.addControl(typeControl, kakao.maps.ControlPosition.TOPRIGHT);
         map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+        kakao.maps.event.addListener(map,"tilesloaded",() => {getChargerData(map.getBounds())})
+        kakao.maps.event.addListener(map,"dragstart",()=>{markers.forEach(marker=>{marker.setMap(null);})})
     }
 
     const createMarker = () => {
-        let bounds = map.getBounds();
-        chargerData.forEach((item) => {
-            let marker = new kakao.maps.Marker({map:map,position: new kakao.maps.LatLng(item.lat, item.lng)});
+        chargerData.forEach((charger) => {
+            var marker = new kakao.maps.Marker({position: new kakao.maps.LatLng(charger.lat, charger.lng)});
+            markers.push(marker);
         })
-        kakao.maps.event.addListener(map,"bounds_changed",()=>{getChargerData(bounds.qa,bounds.pa,bounds.ha,bounds.oa);});
+        markers.forEach((marker)=>{marker.setMap(map)})
     }
 
-    useEffect(()=>{
-        createMap();
-        if (map != null){createMarker()};
-    },[])
+    useEffect(()=>{createMap()},[])
 
-    
+    if (map != null){
+        if(chargerData.length > 700){
+            console.log("충전소가 너무 많아요")
+        } else{
+            createMarker();
+        }
+    }
 
-    console.log(chargerData);
     return(
         <div>
             <div id="map" style={{width:'100vw',height:'97vh'}}></div>
